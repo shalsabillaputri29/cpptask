45480996e9360212296b9bab7e5f5135fdf1e9ed
Shalsabilla Aini <shalsabillaaini4@gmail.com>
2025-10-21 08:12:10 +0700

    Add files via upload

---
+++ filecpp.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+struct Nilai {
+    float mtk;
+    float bind;
+    float bing;
+    float ipa;
+};
+
+struct Siswa {
+    string nama;
+    string nisn;
+    string jurusan;
+    Nilai nilai;
+};
+
+
+float hitungNilaiAkhir(Nilai n) {
+    float hasil;
+    hasil = (n.mtk * 0.4) + (n.ipa * 0.3) + (n.bind * 0.2) + (n.bing * 0.2);
+    return hasil;
+}
+
+void tambahData() {
+    ofstream file;
+    file.open("siswa.txt", ios::app);
+    Siswa s;
+
+    cout << "\nMasukkan nama: ";
+    getline(cin, s.nama);
+    cout << "Masukkan NISN: ";
+    getline(cin, s.nisn);
+    cout << "Masukkan jurusan: ";
+    getline(cin, s.jurusan);
+
+    cout << "Nilai Matematika: ";
+    cin >> s.nilai.mtk;
+    cout << "Nilai Bahasa Indonesia: ";
+    cin >> s.nilai.bind;
+    cout << "Nilai Bahasa Inggris: ";
+    cin >> s.nilai.bing;
+    cout << "Nilai IPA: ";
+    cin >> s.nilai.ipa;
+    cin.ignore();
+
+    file << s.nama << " " << s.nisn << " " << s.jurusan << " "
+         << s.nilai.mtk << " " << s.nilai.bind << " " << s.nilai.bing << " " << s.nilai.ipa << endl;
+
+    file.close();
+    cout << "Data berhasil disimpan!\n";
+}
+
+int main() {
+    int pilih;
+
+    do {
+        cout << "\n===== MENU DATA SISWA =====\n";
+        cout << "1. Tambah Data Siswa\n";
+        cout << "2. Tampilkan Semua Data\n";
+        cout << "3. Cari Data Siswa\n";
+        cout << "4. Keluar\n";
+        cout << "Pilih menu (1-4): ";
+        cin >> pilih;
+        cin.ignore();
+
+        if (pilih == 1)
+            tambahData();
+        else if (pilih == 2)
+            tampilData();
+        else if (pilih == 3)
+            cariSiswa();
+        else if (pilih == 4)
+            cout << "Program selesai.\n";
+        else
+            cout << "Pilihan tidak ada!\n";
+
+    } while (pilih != 4);
+
+    return 0;
+}
