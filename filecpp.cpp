60a74689b7c2d41271777602a4249e2efac4e318
Shalsabilla Aini <shalsabillaaini4@gmail.com>
2025-10-21 08:51:36 +0700

    Update filecpp.cpp

--- filecpp.cpp
+++ filecpp.cpp
@@ -17,7 +17,7 @@ struct siswa {
 };
 
 float nilaiAkhir(nilai n) {
-    return (n.Mtk * 0.4) + (n.IPA * 0.3) + (n.BIn * 0.2) + (n.Big * 0.1);
+    return (n.Mtk * 0.4) + (n.IPA * 0.3) + (n.BIn * 0.2) + (n.Big * 0.2);
 }
 
 void tambahDataSiswa() {
@@ -81,13 +81,19 @@ int main() {
         cout << "Pilih menu (1-5): ";
         cin >> pilih;
         switch (pilih) {
-            case 1: tambahDataSiswa(); break;
-            case 2: tampilDataSiswa(); break;
-            case 3: cariSiswa(); break;
-            case 4: ranking(); break;
-            case 5: cout << "Keluar dari program...\n"; break;
+            case 1: tambahDataSiswa(); 
+                break;
+            case 2: tampilDataSiswa(); 
+                break;
+            case 3: cariSiswa(); 
+                break;
+            case 4: ranking(); 
+                break;
+            case 5: cout << "Keluar dari program...\n";
+                break;
             default: cout << "Pilihan tidak valid!\n";
         }
     } while (pilih != 5);
     return 0;
 }
+
